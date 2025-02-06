b2eae6e495ad2af4f2bd238a045dd4e0ea9f6ede
Jongsung <jos8304@hotmail.com>
2025-02-06 11:02:22 -0500

    apply style to login page

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import InputField from '../../components/InputField';
 
 function LoginScreen() {
+  const [values, setValue] = useState({
+    email: '',
+    password: '',
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeText = (name: string, text: string) => {
+    setValue({...values, [name]: text});
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="Email"
+          error={'Invalid email'}
+          touched={touched.email}
+          inputMode="email"
+          value={values.email}
+          onChangeText={text => handleChangeText('email', text)}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputField
+          placeholder="Password"
+          error={'Invalid password'}
+          touched={touched.password}
+          secureTextEntry
+          value={values.password}
+          onChangeText={text => handleChangeText('password', text)}
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+});
 
 export default LoginScreen;
